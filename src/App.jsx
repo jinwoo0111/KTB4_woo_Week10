fbcfe850212969df0f63209deb53a0380cabaa91
jinwoo0111 <whi750105@naver.com>
2026-07-19 22:21:57 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router";
+
+function App() {
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={<h1>RESCENE FAN COMMUNITY</h1>}
+      />
+    </Routes>
+  );
+}
+
+export default App;
